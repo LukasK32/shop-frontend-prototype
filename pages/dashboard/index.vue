64dc2816288a1766a7e07010f38c8dfa6c14c137
Lukas Kirylak <contact@lukask.pro>
2020-05-24 22:17:14 +0000

    <ultimate joke, have fun!>

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>Dashboard</h1>
-    <p>...</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tincidunt, orci et condimentum ornare, sapien massa placerat nisl, a tincidunt mi quam vel dui. Aliquam et odio tempor, porttitor nunc non, sollicitudin ex. Sed vitae posuere lacus, non venenatis tortor. Duis in eros hendrerit, porta tellus ut, tristique quam. In lorem magna, pretium sit amet mollis et, pharetra ut massa. Cras nulla nisi, fermentum eget ipsum ac, laoreet lobortis neque. Mauris a pharetra magna. Cras vitae imperdiet urna. Maecenas sit amet lacus quam. Ut et lacinia tortor. </p>
   </div>
 </template>
 
